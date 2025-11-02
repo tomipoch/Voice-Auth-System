01384b949dd7fdffdf474e61b77a8e5e119e40db
Tomás Poblete <78375649+tomipoch@users.noreply.github.com>
2025-11-02 13:03:48 -0300

    refactor: Remove unused components, services, and routing
    - Deleted app routing file and associated routes.
    - Removed styles and HTML for app, authentication, dashboard, and enrollment components.
    - Eliminated spec files for testing components and services.
    - Removed API and audio services along with their functionalities.
    - Deleted environment configuration and main application files.
    - Cleaned up TypeScript configuration files.

--- Frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8001', // URL del servidor de desarrollo
-  apiUrlProd: 'http://localhost:8000' // URL del servidor en Docker
-};
